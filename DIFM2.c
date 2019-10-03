95ec8cdfb56278c4de4ab48f3f1676df4e0ec57f
Shashank Kumar <shashank.cs18@bmsce.ac.in>
2019-10-03 15:28:04 +0530

    Create DIFM2.c

---
+++ DIFM2.c
@@ -0,0 +1,49 @@
+#include <graphics.h>
+
+void main(){
+	int i, n, bits[10];
+	int gdriver = DETECT, gmode;
+	int xmax, ymax;
+	int up = 1;
+
+	initgraph(&gdriver, &gmode, "C:\\TURBOC3\\BGI");
+	setcolor(getmaxcolor());
+
+	xmax = getmaxx();
+	ymax = getmaxy();
+
+	line(0, ymax/2, xmax, ymax/2);
+	line(0, 0, 0, ymax);
+
+	printf("Enter size: ");
+	scanf("%d", &n);
+
+	setlinestyle(DASHED_LINE, 1, 1);
+
+	for(i = 0; i <=n; i++)
+		line(i * 50, 0, i * 50, ymax);
+
+	printf("Enter the digital code: ");
+	for(i = 0; i<n; i++)
+		scanf("%d", &bits[i]);
+
+	lineto(0, ymax/2 - 50);
+
+	setcolor(10);
+
+	setlinestyle(SOLID_LINE, 1, 1);
+
+	for(i = 0; i<n; i++){
+		if(bits[i] == 0){
+			linerel(0, -100 + 200 * up);
+			up = up ? 0 : 1;
+		}
+		linerel(25, 0);
+		linerel(0, -100 + 200 * up);
+		up = up ? 0 : 1;
+		linerel(25, 0);
+	}
+	
+	getch();
+	closegraph();
+}
